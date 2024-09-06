12d34d0c4811ba13c6c50fc17472c5a38fafe0ec
戴金鹏 <291691522@qq.com>
2024-09-06 09:03:54 +0800

    Filtered snapshot of check_deviceSN_data at b9e1b8c35c1e3741511b35b896195ad3c38efe6d
    Original ref: refs/remotes/origin/check_deviceSN_data
    Original commit: b9e1b8c35c1e3741511b35b896195ad3c38efe6d
    Original subject: update
    Excluded: *.csv, files >= 1 MiB

---
+++ config/config.py
@@ -0,0 +1,9 @@
+# debug mode
+debug = False
+# request interval (second)
+sleep_time = 0
+# domain name, do not modify it unless necessary.
+domain = 'https://www.foxesscloud.com'
+# your key
+# a1013
+key = 'admeyJpZCI6IjU4MjBhYjljLTJkYjMtNGZkYi1iOTQ1LTc5MTA3ZWMxYjU4NSIsInNlY3JldCI6ImQ5OGU5Y2I5NzMwNmU3YjYzN2EzZjg3ZTZhZDExZTY0MzcwYjA1YzJkN2M0NjZkMTNjY2Y0NjM1MmIxOGZiNDQiLCJwYXlsb2FkIjoiWXlkRmZJWGZnZHRPcnN3NjkxY0lmNGFudjM4N1RvU2p5SUY0d216akhxemd5MUhUeVllaVhXczZnOGZYaVpBV3NUVDF1b2xMTjdVRm9ITFZkWEhKSDRCSU9CRGFheklXWVNQNGNSeklKWmlKQjFMQVdkMXFRSDBUR0JTS2ppMGpST1l3YURVWjNFdlRxWVZUalh4cXVqZGt3M3NNQS8rWUFTNnRqNnA1eWVPbFhJSmsySjhlbEw0ci9TbFBXdHJWWEc0TzJ4NjQ2STk1ZFBIWW9FNTBTMTZlMTdiRGN1SGh4bklqSEUxekRPK0dNVnpRYjJ1TFpOeHFvZFlqb3NtRCJ9'
